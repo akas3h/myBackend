e5db00570d3a945059f288dc7ca67c5317d46538
Akash Kumar <akash565722@gmail.com>
2025-08-21 08:27:26 +0530

    utils setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credintials:true
+}))
+
+// for taking json data
+app.use(express.json({limit: "16kb"}))
+
+//fot taking file
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// for storing file in public like images, favicon
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+export { app }
